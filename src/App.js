02f0bef83ee48dcb9201985b1cce2ca08e5e3880
zullum <zullumsan@gmail.com>
2022-03-16 23:32:03 +0100

    Added sidebar.

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="users">
+              <Route index element={<List />}></Route>
+              <Route path=":userId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+            <Route path="products">
+              <Route index element={<List />}></Route>
+              <Route path=":productId" element={<Single />}></Route>
+              <Route path="new" element={<New />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
